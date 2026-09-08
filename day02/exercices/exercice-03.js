4997562f2802b754aea4cdec132a281fb4dcff5f
cherdal achraf <chrdalachraf85@gmail.com>
2026-09-08 17:21:19 +0100

    day 2 exercice 1 to 4

--- day02/exercices/exercice-03.js
+++ day02/exercices/exercice-03.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let nombre = 7 ;
+for(let i = 1;i <= 10 ; i++){
+    console.log(i + " * 7" + " = " + i * nombre)   
+}
--- day02/exercices/exercice-04.js
+++ day02/exercices/exercice-04.js
@@ -17,4 +17,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let sum = 0;
+for(let i = 1 ;i <=50 ; i++){
+    sum += i;
+}
+console.log(sum)
